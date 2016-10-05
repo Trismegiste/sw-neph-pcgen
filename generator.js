ffc9adc25da1163c9f864c97c0186ff9af6bdea7
Trismegiste <flo@trismegiste.net>
2016-10-05 07:50:12 +0200

    age ok

--- generator.js
+++ generator.js
@@ -2,7 +2,7 @@ var generator = {
     init: function () {
         $('button').click(function (e) {
             var obj = generator.generate();
-            $('#export').text(JSON.stringify(obj));
+            $('#export').text(JSON.stringify(obj, null, 2));
             return false;
         });
     },
@@ -11,6 +11,12 @@ var generator = {
 
         // gender
         nc.gender = (Math.random() > 0.5) ? 'M' : 'F';
+        // age
+        var ageChoice = this.rollAge();
+        nc.age = ageChoice.age();
+        nc.emergencyTime = ageChoice.emergencyTime;
+        nc.activity = ageChoice.label;
+
 
         // attributes
         for (var k = 0; k < 5; k++) {
@@ -21,5 +27,42 @@ var generator = {
 
 
         return nc;
+    },
+    ageModel: [
+        {proba: 1, label: 'Élève primaire', emergencyTime: '1 h', age: function () {
+                return dice.roll(6) + 5;
+            }},
+        {proba: 2, label: 'Collégien', emergencyTime: '4 h', age: function () {
+                return dice.roll(6) + 10;
+            }},
+        {proba: 3, label: 'Lycéen – étudiant', emergencyTime: '2 j', age: function () {
+                return dice.roll(4) + dice.roll(4) + 14;
+            }},
+        {proba: 9, label: 'Adulte', emergencyTime: '1 semaine', age: function () {
+                return dice.roll(20) + dice.roll(20) + 18;
+            }},
+        {proba: 5, label: 'Retraité', emergencyTime: '1 mois', age: function () {
+                return dice.roll(10) + dice.roll(10) + dice.roll(10) + 50;
+            }}
+    ],
+    rollAge: function () {
+        var total = 0;
+        this.ageModel.forEach(function (row) {
+            total += row.proba;
+        });
+
+        var choice = dice.roll(total);
+        var current = 0;
+        var found;
+        this.ageModel.forEach(function (row) {
+            if (found === undefined) {
+                current += row.proba;
+                if (current >= choice) {
+                    found = row;
+                }
+            }
+        });
+
+        return found;
     }
 };
