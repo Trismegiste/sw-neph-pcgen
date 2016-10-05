c194f87cf4148e0b51d608f3ccfb3d22bf5d5768
Trismegiste <flo@trismegiste.net>
2016-10-05 06:56:15 +0200

    poc ok

--- Character.js
+++ Character.js
@@ -5,5 +5,34 @@
 var Character = function () {
     this.attribute = {};
     this.skill = {};
+    this.gender = '';
+    this.age = 0;
+
+    var that = this;
+    savageWorlds.trait.attribute.forEach(function (attr) {
+        that.attribute[attr] = 4;
+    });
+
+    this.increaseAttribute = function (attr) {
+        this.attribute[attr] += 2;
+        if (this.attribute[attr] > 12) {
+            this.attribute[attr] = 12;
+        }
+    };
+
+    this.decreaseAttribute = function (attr) {
+        this.attribute[attr] -= 2;
+        if (this.attribute[attr] < 4) {
+            this.attribute[attr] = 4;
+        }
+    };
+
+    this.increaseSkill = function (skill) {
+        if (this.skill[skill] === undefined) {
+            this.skill[skill] = 4;
+        } else {
+            this.skill[skill] += 2;
+        }
+    };
 };
 
--- generator.js
+++ generator.js
@@ -1,15 +1,24 @@
 var generator = {
     init: function () {
         $('button').click(function (e) {
-            alert('toto');
+            var obj = generator.generate();
+            $('#export').text(JSON.stringify(obj));
             return false;
         });
     },
     generate: function () {
         var nc = new Character();
-        savageWorlds.trait.attribute.forEach(function (attr) {
-            nc.attribute[attr] = 4;
-        });
+
+        // gender
+        nc.gender = (Math.random() > 0.5) ? 'M' : 'F';
+
+        // attributes
+        for (var k = 0; k < 5; k++) {
+            var attr = savageWorlds.trait.attribute[Math.floor(Math.random() * 5)];
+            nc.increaseAttribute(attr);
+        }
+        // skills
+
 
         return nc;
     }
